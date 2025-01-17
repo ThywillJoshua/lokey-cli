7a3605af47b1e286235ad10ab618f0dfd305d0f4
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2025-01-17 13:20:27 +0100

    add handlers folder

--- server/server.go
+++ server/server.go
@@ -3,14 +3,11 @@ package server
 
 import (
 	"embed"
-	"encoding/json"
 	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"translate-cli/globals"
+	"translate-cli/handlers"
 )
 
 // Embed static files
@@ -25,71 +22,14 @@ func StartHTTPServer() {
 
 	http.Handle("/", http.FileServer(http.FS(webapp)))
 
-    http.HandleFunc("GET /files/", getFiles)
+    http.HandleFunc("GET /files/", handlers.GetFiles)
 
-	http.HandleFunc("GET /config/", getConfig)
+	http.HandleFunc("GET /config/", handlers.GetConfig)
 
-	http.HandleFunc("/close", closeHandler)
+	http.HandleFunc("/close", handlers.CloseApp)
+
+	
 	fmt.Println("Starting server on http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func closeHandler(w http.ResponseWriter, r *http.Request) {
-	os.Exit(0)
-}
-
-func getConfig (w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(globals.ConfigData.Config); err != nil {
-		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
-	}
-}
-
-func getFiles (w http.ResponseWriter, r *http.Request) {
-	files, err := getAllFiles(globals.ConfigData.Config.FilesPath)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to list files: %v", err), http.StatusInternalServerError)
-		return
-	}
-
-	var result []map[string]interface{}
-
-	for _, file := range files {
-		filePath := filepath.Join(globals.ConfigData.Config.FilesPath, file)
-		content, err := os.ReadFile(filePath)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Failed to read file %s: %v", file, err), http.StatusInternalServerError)
-			return
-		}
-
-		var jsonContent interface{}
-		if err := json.Unmarshal(content, &jsonContent); err != nil {
-			http.Error(w, fmt.Sprintf("Failed to parse JSON content in file %s: %v", file, err), http.StatusInternalServerError)
-			return
-		}
-
-		result = append(result, map[string]interface{}{
-			"fileName": file,
-			"content": jsonContent,
-		})
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(result); err != nil {
-		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
-	}
-}
-
-func getAllFiles(dirPath string) ([]string, error) {
-    var files []string
-    err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if !info.IsDir() && filepath.Ext(path) == ".json" {
-            files = append(files, filepath.Base(path))
-        }
-        return nil
-    })
-    return files, err
-}
