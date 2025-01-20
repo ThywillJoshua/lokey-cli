b9bd9b04433d4c2347243e1939fb55e0ac1a7349
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2025-01-20 21:28:57 +0100

    Add key value placeholder

--- server/static/server/angular-app-manifest.mjs
+++ server/static/server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 1201, hash: 'f5ad6312ba6d571542d99356318a0aa5486a5ee9b3b9f46cd44d696658b891a2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1129, hash: 'f45edd8cc00f1a4d038c079e76314be8644f62caade93b2feca002df79a66700', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1201, hash: 'b694c110b4ac49464d5c5780772aaa8c0200e12bdd1682c83cb151b34ae7ddcd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1129, hash: 'f390164cf1b8806cef35365e4c7d953d72233c1330d32c47b3a45c2552ebe188', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-22CYXLPO.css': {size: 5588, hash: 'RudPFWZxFlU', text: () => import('./assets-chunks/styles-22CYXLPO_css.mjs').then(m => m.default)}
   },
 };
