d20000c1a5ab2ef1795e1cc5f67ead581147baf2
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2025-01-21 14:18:27 +0100

    Remove files service

--- ui/src/app/shared/services/files/files.service.ts
+++ ui/src/app/shared/services/files/files.service.ts
@@ -52,26 +52,4 @@ export class FilesService {
       )
       .subscribe();
   }
-
-  static sortObjectKeys(
-    obj: Record<string, any>,
-    order: 'ASC' | 'DESC' = 'ASC'
-  ): Record<string, any> {
-    // Sort the keys based on the provided order
-    const sortedKeys = Object.keys(obj).sort((a, b) => {
-      if (order === 'ASC') {
-        return a.localeCompare(b); // Ascending order
-      } else {
-        return b.localeCompare(a); // Descending order
-      }
-    });
-
-    // Create a new object with sorted keys
-    const sortedObject: Record<string, any> = {};
-    for (const key of sortedKeys) {
-      sortedObject[key] = obj[key];
-    }
-
-    return sortedObject;
-  }
 }
